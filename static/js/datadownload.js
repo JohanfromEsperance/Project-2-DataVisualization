d69b977967e0ced985ba77a5eb1b9ef771c64c1e
vn02063007 <113915639+vn02063007@users.noreply.github.com>
2023-01-23 23:05:25 +0800

    Update datadownload.js

--- static/js/datadownload.js
+++ static/js/datadownload.js
@@ -1,12 +1,12 @@
 document.getElementById("download-button").addEventListener("click", function(){
     var link = document.createElement('a');
-    link.href = 'file:///C:/Users/Tom%20B/Desktop/module/project%203/test%20website/data.json';
+    link.href = 'https://github.com/JohanfromEsperance/Project-3-DataVisualization/blob/main/static/file/data.json';
     link.download = 'data.json';
     link.dispatchEvent(new MouseEvent('click'));
 });
 document.getElementById("download-button2").addEventListener("click", function(){
     var link = document.createElement('a');
-    link.href = 'file:///C:/Users/Tom%20B/Desktop/module/project%203/test%20website/new_master_data.csv';
+    link.href = 'https://github.com/JohanfromEsperance/Project-3-DataVisualization/blob/main/static/file/new_master_data.csv';
     link.download = 'new_master_data.csv';
     link.dispatchEvent(new MouseEvent('click'));
 });
