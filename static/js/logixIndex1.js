8530bb2f0458736e43d1c1ae04143b26b07591db
Johan Snyman <112832608+JohanfromEsperance@users.noreply.github.com>
2023-01-28 09:18:35 +0800

    Update logixIndex1.js

--- static/js/logixIndex1.js
+++ static/js/logixIndex1.js
@@ -1,18 +1,17 @@
 // Define quakemap plates GeoJSON url variable
-//https://earthquake.usgs.gov/earthquakes/feed/v1.0/geojson.php
-// var url = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geojson";
+
+
 //var url = "new_master_datamon2323.geojson";
-var url2 = "new_master_data1.json";
+//var url2 = "new_master_data1.json";
 
 var url = "static/data/new_master_datamon2323.geojson";
+var url2 = "static/new_master_data1.json";
 
-//var url ="https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/significant_hour.geojson";
-//var url ="https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_hour.geojson";
-
-// Create earthquake layerGroup
+// Create layerGroup
 let quakemap = L.layerGroup();
 //Direct copy from the sample code in //docs.mapbox.com/api/maps/static-images
 // Create tile layer and add copyright noices from Website
+
 var grayscaleMap = L.tileLayer("https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
   attribution: "© <a href='https://www.mapbox.com/about/maps/'>Mapbox</a> © <a href='http://www.openstreetmap.org/copyright'>OpenStreetMap</a> <strong><a href='https://www.mapbox.com/map-feedback/' target='_blank'>Improve this map</a></strong>",
   tileSize: 512,
@@ -23,8 +22,6 @@ var grayscaleMap = L.tileLayer("https://api.mapbox.com/styles/v1/{id}/tiles/{z}/
 });
 //mapbox://styles/mapbox/satellite-v9
 //mapbox://styles/mapbox/outdoors-v12
-// Create the map, giving it the grayscaleMap and quakemap layers to display on load
-// Used Cairo as the centre to align all the circles in a full screen
 
 d3.json(url,function(data2) {console.log(data2);});
 d3.json(url2,function(data3) {console.log(data3);});
@@ -37,34 +34,6 @@ let myMap = L.map("mapid", {
   zoom: 16,
   layers: [grayscaleMap, quakemap]
 });
-
-////// for the heatmapl cluster layer lesson 2 week 15 Solved 
-//////Get the data with d3.
-/////d3.json(url2),(function(response) {
-
-  ///// Create a new marker cluster group.
-  /////let markers = L.markerClusterGroup();
-
-  ///// Loop through the data.
-  /////for (let i = 0; i < response.length; i++) { {console.log(response)};
-
-    ///// Set the data location property to a variable.
-    /////let location = response[i].Latitude;
-
-    ///// Check for the location property.
-   ///// if (location) {
-
-      ///// Add a new marker to the cluster group, and bind a popup.
-      /////markers1.addLayer(L.marker([response[i].Latitude[1], response[i].Latitude[0]])
-      /////  .bindPopup(response[i].Condition_Point[i]));
-   ///// }
-
- ///// };
-
-  // Add our marker cluster layer to the map.
-  //myMap.addLayer(markers);
-
-//});
 
 d3.json(url, function(Data) {
   // Determine the marker size by magnitude
@@ -90,26 +59,8 @@ d3.json(url, function(Data) {
     }
   };
 
-    // JOHAN MADE THSI TO TEST: Create a new marker cluster group.
-    //let markers = L.markerClusterGroup();
-
-    // Loop through the data.
-    //for (let i = 0; i < Data.length; i++) {
-      // Set the data location property to a variable.
-     // let location = Data[i].Latitude;
-      // Check for the location property.
-     // if (location) {
-        // Add a new marker to the cluster group, and bind a popup.
-        //markers1.addLayer(L.marker([Data[i].Latitude[1], Data[i].Latitude[0]])
-        //  .bindPopup(Data[i].Condition_Point[i]));
-      //}
-    //};
-    // Add our marker cluster layer to the map.
-
- /// myMap.addLayer(markers);
-
   // Create the GeoJSON layer
-  // Each feature a popup describing the place and time of the earthquake
+  // Each feature a popup describing the Asset
   //Lesson 3 Copy code and applications from studies
   L.geoJSON(Data, {
     pointToLayer: function (feature, latlng) {
@@ -135,8 +86,6 @@ d3.json(url, function(Data) {
     }
   }).addTo(quakemap);
 
-
-  // Sending our quakemap layer to the createMap function
 
   quakemap.addTo(myMap);
 
